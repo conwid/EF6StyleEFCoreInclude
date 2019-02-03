dfdb045877df478538bf5fb95aae5a79b0d40838
conwid <akos@dotnetfalcon.com>
2019-02-03 12:06:17 +0100

    Add files via upload

---
+++ InlcudeConverterSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using IncludeConverterSample.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncludeConverterSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (NorthwindContext ctx = new NorthwindContext())
+            {
+                var query = ctx.Categories.Include(c => c.Products.Select(p => p.OrderDetails.Select(od => od.Order.Customer)));
+                var x = await query.ToListAsync();
+            }
+            Console.ReadLine();
+        }
+    }
+}
